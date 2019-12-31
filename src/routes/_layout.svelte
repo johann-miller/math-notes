db1f3eb5734ef4b25d138a1721fb83243495d57d
johann-miller <johann.miller@protonmail.com>
2019-12-31 03:08:25 -0500

    Added MathJax support

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,7 +3,12 @@
 
 <style>
 	main {
-		
+		align-items: center;
+		display: flex;
+		flex-flow: column;
+		justify-content: flex-start;
+		min-width: 100vw;
+		min-height: 100vh;
 	}
 </style>
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,36 @@
-<style>
+<script id="MathJax-script">
+	import { onMount, afterUpdate } from 'svelte'
 
+	function redrawMath() {
+		let script = document.createElement('script')
+    	script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"
+    	document.head.append(script)
+		
+		script.onload = () => {
+			MathJax = {
+				tex: {inlineMath: [['$', '$'], ['\\(', '\\)']]},
+				svg: {fontCache: 'global'}
+			}
+		}
+	}
+	
+	onMount(() => {
+		redrawMath()
+	})
+
+	afterUpdate(() => {
+		redrawMath()
+	})
+	
+</script>
+
+<style>
+	h1 {
+		line-height: 1.5;
+		padding: 1.5rem 0 0 0;
+		width: 100%;
+		text-align: center;
+	}
 </style>
 
 <svelte:head>
@@ -7,3 +38,5 @@
 </svelte:head>
 
 <h1>Math notes</h1>
+
+<p>$$x = y$$</p>
