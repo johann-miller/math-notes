4c229f44778695a4a9e8116df9b827ee394a1627
johann-miller <johann.miller@protonmail.com>
2020-01-18 19:06:48 -0500

    Added mathjax rendering to article page

--- src/routes/article/[slug].svelte
+++ src/routes/article/[slug].svelte
@@ -1,33 +1,14 @@
-<script context="module">
-    // export async function preload(page, session) {
-    //     const { slug } = page.params
-    //     const db = firebase.firestore()
-    //     const articleRef = db.collection('articles').doc({ slug }.slug)
-    //     let { article } = ''
-        
-    //     articleRef.get()
-    //         .then(function(doc) {
-    //             if(doc.exists) {
-    //                 console.log(doc.data())
-    //                 article = doc.data()
-    //             }
-    //         })
-
-    //     return { article }
-    // }
-</script>
-
 <script>
     import { stores } from "@sapper/app"
-    import { onMount } from 'svelte'
+    import { onMount, afterUpdate } from 'svelte'
     let db, article
     const { page } = stores()
     const { slug } = $page.params
 
+    // Gets article from database
     onMount(() => {
         db = firebase.firestore()
         const articleRef = db.collection('articles').doc(slug)
-        console.log(slug)
 
         articleRef.get().then(function(doc) {
             if(doc.exists) {
@@ -36,14 +17,39 @@
         })
     })
 
+    // Rerender any math after the article content has been updated
+    afterUpdate(() => {
+        if (window.MathJax) {
+            window.MathJax.Hub.Config({
+                tex2jax: {
+                    inlineMath: [["$", "$"], ["(", ")"]],
+                    displayMath: [["$$", "$$"], ["[", "]"]],
+                    processEscapes: true,
+                    processEnvironments: true
+                },
+                displayAlign: "center",
+                "HTML-CSS": {
+                    styles: { ".MathJax_Display": { margin: 0 } },
+                    linebreaks: { automatic: true }
+                }
+            });
+        }
+
+    if (window.MathJax) {
+      window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub, "article"]);
+    }
+    })
+
     function date(timestamp) {
         let date = new Date(timestamp * 1000).toDateString();
         return date
     }
 </script>
 
-{#if article}
-<h1>{article.title}</h1>
-<span>{date(article.created.seconds)}</span><br/>
-{@html article.body}
-{/if}
+<article id="article">
+    {#if article}
+        <h1>{article.title}</h1>
+        <span>{date(article.created.seconds)}</span><br/>
+        {@html article.body}
+    {/if}
+</article>
--- src/routes/editor/index.svelte
+++ src/routes/editor/index.svelte
@@ -1,127 +1,122 @@
 <script id="MathJax-script">
-	import { onMount, afterUpdate } from 'svelte'
+  import { onMount, afterUpdate } from "svelte";
 
-	let input = ''
-	let idleInput
+  let input = "";
+  let idleInput;
 
-	function inputChange() {
-		clearTimeout(idleInput)
-		idleInput = setTimeout(updateOutput, 750)
-	}
+  function inputChange() {
+    clearTimeout(idleInput);
+    idleInput = setTimeout(updateOutput, 750);
+  }
 
-	function updateOutput() {
-		document.getElementById("output").innerHTML = input
+  function updateOutput() {
+    document.getElementById("output").innerHTML = input;
 
-		if (window.MathJax) {
-			window.MathJax.Hub.Queue([
-				'Typeset',
-				window.MathJax.Hub,
-				'output'
-			])
-		}
-	}
+    if (window.MathJax) {
+      window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub, "output"]);
+    }
+  }
 
-	onMount(() => {
-		if (window.MathJax) {
-			window.MathJax.Hub.Config({
-				tex2jax: {
-					inlineMath: [['$', '$'], ['(', ')']],
-					displayMath: [['$$', '$$'], ['[', ']']],
-					processEscapes: true,
-					processEnvironments: true
-				},
-				displayAlign: 'center',
-				'HTML-CSS': {
-					styles: { '.MathJax_Display': { margin: 0 } },
-					linebreaks: { automatic: true }
-				},
-			})
-		}
-	})
+  onMount(() => {
+    if (window.MathJax) {
+      window.MathJax.Hub.Config({
+        tex2jax: {
+          inlineMath: [["$", "$"], ["(", ")"]],
+          displayMath: [["$$", "$$"], ["[", "]"]],
+          processEscapes: true,
+          processEnvironments: true
+        },
+        displayAlign: "center",
+        "HTML-CSS": {
+          styles: { ".MathJax_Display": { margin: 0 } },
+          linebreaks: { automatic: true }
+        }
+      });
+    }
+  });
 </script>
 
 <style>
-	.container {
-		height: calc(100vh - 3rem);
-		width: 100%;
-		display: flex;
-		flex-flow: column;
-		align-items: center;
-	}
+  .container {
+    height: calc(100vh - 3rem);
+    width: 100%;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+  }
 
-	.editor-container {
-		display: grid;
-		grid-area: editor;
-		grid-template-columns: repeat(2, 1fr);
-		grid-template-rows: 1fr;
-		grid-template-areas:
-			"input output";
-		width: 100%;
-		height: 100%;
-	}
+  .editor-container {
+    display: grid;
+    grid-area: editor;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 1fr;
+    grid-template-areas: "input output";
+    width: 100%;
+    height: 100%;
+  }
 
-	.editor-input {
-		border-right: 1px #d6d9dc solid;
-		display: flex;
-		flex-flow: column;
-		grid-area: input;
-	}
+  .editor-input {
+    border-right: 1px #d6d9dc solid;
+    display: flex;
+    flex-flow: column;
+    grid-area: input;
+  }
 
-	.editor-output {
-		grid-area: output;
-	}
+  .editor-output {
+    grid-area: output;
+  }
 
-	#math-input {
-		flex-grow: 1;
-		border: none;
-		font-family: 'Roboto', sans-serif;
-		font-size: 18px;
-		color: inherit;
-		resize: none;
-		width: 100%;
-		max-width: 40rem;
-		padding: 1rem;
-		line-height: 1.8;
-	}
+  #math-input {
+    flex-grow: 1;
+    border: none;
+    font-family: "Roboto", sans-serif;
+    font-size: 18px;
+    color: inherit;
+    resize: none;
+    width: 100%;
+    max-width: 40rem;
+    padding: 1rem;
+    line-height: 1.8;
+  }
 
-	#output {
-		width: 100%;
-		max-width: 40rem;
-		padding: 1rem;
-	}
+  #output {
+    width: 100%;
+    max-width: 40rem;
+    padding: 1rem;
+  }
 
-	.section-area {
-		display: flex;
-		flex-flow: column;
-		align-items: center;
-		line-height: 1.8;
-		font-family: 'Roboto', sans-serif;
-		font-size: 18px;
-		width: 100%;
-		min-height: 100%;
-	}
+  .section-area {
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    line-height: 1.8;
+    font-family: "Roboto", sans-serif;
+    font-size: 18px;
+    width: 100%;
+    min-height: 100%;
+  }
 
-	.section-title-bar {
-		font-weight: 300;
-		width: 100%;
-		padding: 0 1rem;
-		border-bottom: 1px #d6d9dc solid;
-	}
+  .section-title-bar {
+    font-weight: 300;
+    width: 100%;
+    padding: 0 1rem;
+    border-bottom: 1px #d6d9dc solid;
+  }
 </style>
 
 <svelte:head>
-	<title>LaTeX editor</title>
+  <title>LaTeX editor</title>
 </svelte:head>
 
 <div class="container">
-	<div class="editor-container">
-		<section class="editor-input section-area">
-			<div class="section-title-bar">Input</div>
-			<textarea id="math-input" bind:value={input} on:input={inputChange}></textarea>
-		</section>
-		<section class="editor-output section-area">
-			<div class="section-title-bar">Preview</div>
-			<article id="output"></article>
-		</section>
-	</div>
+  <div class="editor-container">
+    <section class="editor-input section-area">
+      <div class="section-title-bar">Input</div>
+      <textarea id="math-input" bind:value={input} on:input={inputChange} />
+    </section>
+    <section class="editor-output section-area">
+      <div class="section-title-bar">Preview</div>
+      <article id="output" />
+    </section>
+  </div>
 </div>
